36995a692441973e802954b7d4d30344e3207d4f
Hapusny <y2324056693@163.com>
2026-07-16 16:28:52 +0800

    docs: GlyphSystemDesign

---
+++ Glyph/Source/Glyph/Public/Glyph/C_GlyphBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "C_GlyphBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GLYPH_API UC_GlyphBase : public UObject
+{
+	GENERATED_BODY()
+	
+};
